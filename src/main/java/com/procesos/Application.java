c03da33ee290a54c2604b20a25ddc7fba6caeac1
Camilo Pinzon <capinzonm@ufpso.edu.co>
2023-06-22 17:56:38 -0500

    Documentación de la API

--- src/main/java/com/procesos/Application.java
+++ src/main/java/com/procesos/Application.java
@@ -1,5 +1,8 @@
 package com.procesos;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -19,5 +22,15 @@ public class Application {
     public RestTemplate restTemplate(RestTemplateBuilder builder){
 
         return builder.build();
+    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API DE USUARIOS Y PRODUCTOS")
+                        .version("1.0.0")
+                        .description("Api rest con operaciones CRUD de usuarios y productos ")
+                        .termsOfService("https://example.com/terms")
+                        .license(new License().name("Apache 2.0").url("https://example.com/license")));
     }
 }
